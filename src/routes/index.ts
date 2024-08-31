7b18fb7e0ddca2bf90ba6b13718196b2f533d8fa
gorushkin <gorushkin@gmail.com>
2024-08-31 20:13:54 +0300

    refactor: Update routes to use prefix for currency rates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,20 +2,19 @@ import { FastifyInstance } from 'fastify';
 import { config } from '../config';
 import { ratesController } from '../controllers/RatesController';
 
+const PREFIX = '/currency-rates';
+
 enum ROUTES {
   ROOT = '/',
   CBRF = '/cbrf',
   COINGATE = '/coingate',
 }
 
-const coingateRoutes = async (app: FastifyInstance) => {
+const ratesRoutes = async (app: FastifyInstance) => {
+  app.get(ROUTES.CBRF, (req, res) => ratesController.getCBRFRates(req, res));
   app.get(ROUTES.COINGATE, (req, res) =>
     ratesController.getCoingateRates(req, res),
   );
-};
-
-const cbRoutes = async (app: FastifyInstance) => {
-  app.get(ROUTES.CBRF, (req, res) => ratesController.getCBRFRates(req, res));
 };
 
 const testRoutes = async (app: FastifyInstance) => {
@@ -25,9 +24,6 @@ const testRoutes = async (app: FastifyInstance) => {
 };
 
 export const routes = async (app: FastifyInstance) => {
-  app.register(coingateRoutes);
-
-  app.register(cbRoutes);
-
+  app.register(ratesRoutes, { prefix: PREFIX });
   app.register(testRoutes);
 };
