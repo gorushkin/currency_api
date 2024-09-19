d0c93dc11193962a60a76b2f5f790848bc10f509
gorushkin <gorushkin@gmail.com>
2024-09-19 21:36:48 +0300

    refactor: Update date format in CBRF API endpoint, add different dates to fetch
    - Updated the date format in the CBRF API endpoint to match the required format.
    - Added the ability to fetch currency rates for different dates.
    
    Refs: #123

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,10 @@
 import Fastify from 'fastify';
 import { routes } from '../routes';
+import cors from '@fastify/cors';
 
 const app = Fastify({ logger: false });
+
+app.register(cors, {});
 
 app.register(routes);
 
