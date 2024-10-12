3236a0c350ef8414344b8071cf7d36454d2ec5ea
gorushkin <gorushkin@gmail.com>
2024-10-12 09:46:54 +0300

    test

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,14 +2,15 @@ import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { db } from './db';
 
 const applyMigrations = async () => {
-  try {
-    await migrate(db, {
-      migrationsFolder: './src/db/migrations',
-    });
-    console.log('Migrations applied');
-  } catch (error) {
-    console.log('error: ', error);
-  }
+  console.log('we do nothing here!!!!');
+  // try {
+  //   await migrate(db, {
+  //     migrationsFolder: './src/db/migrations',
+  //   });
+  //   console.log('Migrations applied');
+  // } catch (error) {
+  //   console.log('error: ', error);
+  // }
 };
 
 applyMigrations();
